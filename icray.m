fecc125cf81c8517022c8a0fac3eb3edcccc6a43
hmalawi <72355025+hmalawi@users.noreply.github.com>
2021-09-27 14:05:23 -0400

    Style updates

--- icray.m
+++ icray.m
@@ -1,17 +1,18 @@
-function [corelat,corelon,coredep,epid,p,turnpt] = ...
-    icray(eqlat,eqlon,eqdepth,stalat,stalon,mod,vphase)
-% [corelat,corelon,coredep,epid,p,turnpt] = ...
-%                      icray(eqlat,eqlon,eqdepth,stalat,stalon,mod,vphase);
+function [corelat, corelon, coredep, epid, p, turnpt] = ...
+    icray(eqlat, eqlon, eqdepth, stalat, stalon, mod, vphase)
+% [corelat, corelon, coredep, epid, p, turnpt] = ...
+%                icray(eqlat, eqlon, eqdepth, stalat, stalon, mod, vphase);
 %
 % This function is mainly built to calculate the discretized path
 % of the inner core segment of a ray.
 %
 % INPUT:
 %
-% eqlat           Earthquake's latitude
-% eqlon           Earthquake's longitude
-% stalat          Station's latitude
-% stalon          Station's longitude	
+% eqlat           Earthquake's latitude [defaulted]
+% eqlon           Earthquake's longitude [defaulted]
+% eqdepth         Depth of earthquakes origin in km [defaulted]
+% stalat          Station's latitude [defaulted]
+% stalon          Station's longitude [defaulted]	
 % mod             The chosen velocity model [defaulted]
 % vphase          Seismic phase (i.e., PKIKP for the innercore) [defaulted]
 %
@@ -28,74 +29,75 @@ function [corelat,corelon,coredep,epid,p,turnpt] = ...
 %
 % Requires TAUPPATH from https://github.com/g2e/seizmo/tree/master/mattaup
 %
-% Written by Huda Al Alawi - May 16th, 2021.
-% Last modified by Huda Al Alawi - July 2nd, 2021
+% Written by Huda Al Alawi - May 16th, 2021
+% Last modified by Huda Al Alawi - September 27th, 2021
 %
 
 % Define default values
-defval('mod','ak135')
-defval('vphase','PKIKP')
+defval('mod', 'ak135')
+defval('vphase', 'PKIKP')
 
 % Default to a reasonable earthquake 
-defval('eqlat',-50)
-defval('eqlon',40)
-defval('eqdepth',randi(440))
+defval('eqlat', -50)
+defval('eqlon', 40)
+defval('eqdepth', randi(440))
 % Default station is Guyot Hall
-defval('stalat',40.34585)
-defval('stalon',-74.65475)
+defval('stalat', 40.34585)
+defval('stalon', -74.65475)
 
 % Now let's use taup to discretize some parameters
-data=tauppath('mod',mod,'dep',eqdepth,'ph',vphase,'sta',[stalat stalon],...
-     'evt',[eqlat eqlon]);
+data = tauppath('mod', mod, 'dep', eqdepth, 'ph', vphase, 'sta', ...
+    [stalat stalon], 'evt', [eqlat eqlon]);
  
 % Epicentral distance
-epid=data.distance;
+epid = data.distance;
 % Ray parameter
-p=data.rayparameter;
+p = data.rayparameter;
 % Discretize epicentral distance
-raydis=data.path.distance;
+raydis = data.path.distance;
 % Discretize depth
-raydep=data.path.depth;
+raydep = data.path.depth;
 % Discretize location
-raylat=data.path.latitude;
-raylon=data.path.longitude;
+raylat = data.path.latitude;
+raylon = data.path.longitude;
 
 % Let's find the parameters of the selected Earth model
-a=eval(mod);
+a = eval(mod);
 % What's the radius of the Earth?
-R=max(a.depth);
+R = max(a.depth);
 % To find the angle of incidence using rayparameter, we need to know the
 % velocity at each point, so...
-for index=1:length(raydep)
-  eval(sprintf('vp(index)=%s',sprintf('getfield(%s(''depths'',%f),''vp'');'...
-      ,mod,raydep(index))));
+for index = 1:length(raydep)
+  eval(sprintf('vp(index)=%s', sprintf('getfield(%s(''depths'', %f), ''vp'');'...
+      , mod, raydep(index))));
 end
 % Incidence angle - protected against rounding imaginary numbers
 % check REALIZE for other cases not relevant here
-iang=asin(min(1,p*vp(:)./[R-raydep(:)]*180/pi))*180/pi;
+iang = asin(min(1, p*vp(:)./[R-raydep(:)]*180/pi))*180/pi;
 % This is because, for a value that is ~ 1 (at 90 degree, mostly turning 
 % point), will start to get complex numbers.
 
 % return the turning point info.
-iturn=find(iang==90);
-turnpt=[raylat(iturn),raylon(iturn),raydep(iturn)];
+iturn = find(iang == 90);
+turnpt = [raylat(iturn), raylon(iturn), raydep(iturn)];
 
 % Now, we only need the discretized path for the inner core segment
 % What is the depth of the inner core?
-index=find(a.vs==0);
-icdep=a.depth(index(end)+1);
+index = find(a.vs == 0);
+icdep = a.depth(index(end)+1);
 
 % Data of innercore only...
-inout=find(raydep==icdep);
+inout = find(raydep == icdep);
 % If, for any reason, there is a problem in the ray, i.e., not having two
 % points at the depth of the inner core, return nothing
-if length(inout)~=2
-    corelat=NaN; corelon=NaN; coredep=NaN; turnpt=NaN; epid=NaN; p=NaN;
+if length(inout) ~= 2
+    corelat = NaN; corelon = NaN; coredep = NaN; turnpt = NaN;
+    epid = NaN; p = NaN;
     return
 % If everything went well, return innercore data
 else
-    corelat=[raylat(inout(1):inout(2))];
-    corelon=[raylon(inout(1):inout(2))];
-    coredep=[raydep(inout(1):inout(2))];
+    corelat = [raylat(inout(1):inout(2))];
+    corelon = [raylon(inout(1):inout(2))];
+    coredep = [raydep(inout(1):inout(2))];
 end
 
