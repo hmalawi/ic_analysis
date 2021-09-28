a4c651d9e06c80c925a1c3481df448f79225e528
hmalawi <72355025+hmalawi@users.noreply.github.com>
2021-09-27 20:02:16 -0400

    Style updates

--- icrays3d.m
+++ icrays3d.m
@@ -1,5 +1,6 @@
-function [pts,index]=icrays3d(T,corelat,corelon,coredep,epid,mod,p,turnpt)
-% pts=icrays3d(T,corelat,corelon,coredep,epid,mod,p,turnpt);
+function [pts, index] = ...
+         icrays3d(T, corelat, corelon, coredep, epid, mod, p, turnpt)
+% pts = icrays3d(T, corelat, corelon, coredep, epid, mod, p, turnpt);
 %
 % This function returns a structure that has the kernel-defining-points
 % of an inner core segment of a ray at different shells (depths).
@@ -13,92 +14,96 @@ function [pts,index]=icrays3d(T,corelat,corelon,coredep,epid,mod,p,turnpt)
 % epid         Epicentral distance
 % mod          The chosen velocity model [defaulted]
 % p            Ray parameter [s/deg]
-% turnpt       Latitude,longitude, and depth of the turning point
+% turnpt       Latitude, longitude, and depth of the turning point
 %
 % SEE ALSO:
 %
 % ICRAY
 %
 % Written by Huda Al Alawi - May 20th, 2021.
-% Last modified by Huda Al Alawi - July 2nd, 2021
+% Last modified by Huda Al Alawi - September 27th, 2021
 %
 
 % Define default values
-defval('mod','ak135')
+defval('mod', 'ak135')
 
 % We need to define the width of the kernels based on the dominant period
 % and the epicentral distance (Calvet et al., 2006)
 switch T
     case 1
         if epid>=146 && epid<160
-            width=295;
+            width = 295;
         elseif epid>=160 && epid<170
-            width=365;
+            width = 365;
         elseif epid>=170 && epid<180
-            width=390;
+            width = 390;
         end
        
     case 2
         if epid>=146 && epid<160
-            width=415;
+            width = 415;
         elseif epid>=160 && epid<170
-            width=510;
+            width = 510;
         elseif epid>=170 && epid<180
-            width=540;
+            width = 540;
         end
 end
 
 % The radius of the inner core based on the choosen model...
-a=eval(mod);
-R=max(a.depth);
+a = eval(mod);
+R = max(a.depth);
 % The radius of the inner core will be R-(the first or last element of coredep array)
-rsphere=R-coredep(1);
+rsphere = R-coredep(1);
 
 % A flag for the turning point
-turn=0;
+turn = 0;
 
 % OF COURSE NEED TO INTERPOLATE THE DATA...
 
 % The ray is discretized into small "cylinders". Each time, there will be
 % in and out points. We should:
-for ii=1:length(corelat)-1
+for ii = 1:length(corelat)-1
     % 1. Define these points, then call LINE3SPHERE to find the other
-    % intersection point with the sphere of interest (specific depth of the inner core).
-    th=[corelon(ii), corelon(ii+1)]*pi/180;
-    phi=[corelat(ii), corelat(ii+1)]*pi/180;
-    r=(R-[coredep(ii), coredep(ii+1)])./R;
-    [x,y,z]=sph2cart(th(:),phi(:),r(:));
-    xyz=line3sphere([x(1),y(1),z(1)],[x(2),y(2),z(2)],[0,0,0,(R-coredep(ii))/R],0);
+    % intersection point with the sphere of interest.
+    th = [corelon(ii), corelon(ii+1)] * pi/180;
+    phi = [corelat(ii), corelat(ii+1)] * pi/180;
+    r = (R-[coredep(ii), coredep(ii+1)])./R;
+    [x, y, z] = sph2cart(th(:), phi(:), r(:));
+    xyz = line3sphere([x(1), y(1), z(1)], [x(2), y(2), z(2)], ...
+        [0, 0, 0, (R-coredep(ii))/R], 0);
     
     % Should find the coordinates of the second point to use CYLINDRIC
-    [th,phi,r]=cart2sph(xyz(1,2),xyz(2,2),xyz(3,2));
-    outlon=th*180/pi; outlat=phi*180/pi;
+    [th, phi, r] = cart2sph(xyz(1,2), xyz(2,2), xyz(3,2));
+    outlon = th*180/pi; outlat = phi*180/pi;
     
     % 2. Call CYLINDRIC to find the interection of a cylinder of radius r
     % (ray with kernel width) with a sphere. There will be top patch and bottom
     % patch. Take the top until the turning point, then take the bottom.
     % Radius of sphere and cylinder are normalized (using sphere's radius)
-    [xyzS,topS,botS]=cylindric((width/2)/rsphere,[corelon(ii) corelat(ii)],[outlon outlat],1,0);
+    [xyzS, topS, botS] = cylindric((width/2)/rsphere, [corelon(ii) corelat(ii)], ...
+        [outlon outlat], 1, 0);
     
     % Check if have reached the turning point yet
     % If so, change the flag to 1;
-    if corelat(ii)==turnpt(1) && corelon(ii)==turnpt(2) && coredep(ii)==turnpt(3)
-        turn=1;
-        index=ii;
+    if turn==0 && corelat(ii)==turnpt(1) && corelon(ii)==turnpt(2) && ...
+            coredep(ii)==turnpt(3)
+        turn = 1;
+        index = ii;
     end
     
     % Top or bottom?
     % Check, turn the points and the depths
-    if turn==0
-        pts{ii,1}=topS;
-        pts{ii,2}=coredep(ii);
+    switch turn
+        case 0
+            pts{ii,1} = topS;
+            pts{ii,2} = coredep(ii);
+            
+        case 1
+            pts{ii,1} = botS;
+            pts{ii,2} = coredep(ii+1);
         
-    elseif turn==1
-        pts{ii,1}=botS;
-        pts{ii,2}=coredep(ii+1);
     end
 
 end
-
 
 end
